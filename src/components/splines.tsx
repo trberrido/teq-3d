d38b36f080736ec86573d3e203c97cbb52acb42e
Thibault Berrdo <thibault.berrido@perfectogroupe.fr>
2025-03-26 23:42:12 +0000

    add: english splines

--- src/components/splines.tsx
+++ src/components/splines.tsx
@@ -7,11 +7,11 @@ const splines = {
       { name: 'Superposition', id: 'gMbX5snu32r7ziEy' }
     ],
     en: [
-      { name: 'Young-slit experiment', id: 'otMntQBTlArtVvQP' },
-      { name: 'Entanglement', id: 'CijJKpu1c7oRLv-I' },
-      { name: 'Measurement', id: 'BBcwrXH0Up0Pd98o' },
-      { name: 'Quantification', id: 'h0ZKEquHu1Ur1stC' },
-      { name: 'Superposition', id: 'gMbX5snu32r7ziEy' }
+      { name: 'Young slit experiment', id: 'FveMVSUAUHtQtDh2' },
+      { name: 'Entanglement', id: 'ncPjqIhIinBGdJtM' },
+      { name: 'Measurement', id: 'VrtOwa4vOqTejPs7' },
+      { name: 'Quantification', id: 'f7D32Kpx2R7jRS7r' },
+      { name: 'Superposition', id: '0jfb4yab8oKg-o44' }
     ]
 };
 
